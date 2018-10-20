10492041e6e49110cb809eea31b5704fc154bb60
FiveTech Software <alinares@fivetechsoft.com>
2018-10-20 20:30:50 +0200

    Update xcloud.js

--- source/js/xcloud.js
+++ source/js/xcloud.js
@@ -1,1 +1,89 @@
 // xcloud javascript
+
+function OpenFile()
+{
+   var input = document.getElementById( "myFile" );
+   var reader;
+   
+   if( input.files )
+   {
+      reader = new FileReader();
+     
+      reader.addEventListener( 'load', function( e ) {
+         editor.setValue( e.target.result, -1 ); } );
+         
+      reader.readAsBinaryString( input.files[ 0 ] ); 
+   }
+}   
+  
+function SendFile( cFileName )
+{
+   var formData = new FormData();
+   var xhr = new XMLHttpRequest();
+   var myblob = new Blob( [ editor.getValue() ], {
+                type: 'text/plain' } );
+  
+   formData.append( "test", myblob, "test.prg" );
+   xhr.onreadystatechange = function() { 
+     if( this.readyState == XMLHttpRequest.DONE && this.status == 200 ) {
+        alert( this.responseText );
+    } 
+   };
+   xhr.open( "POST", 'https://www.fivetechsoft.com/xcloud/upload.php' );
+   xhr.send( formData );
+}
+  
+function Run()  
+{  
+   var formData = new FormData();
+   var xhr = new XMLHttpRequest();
+   var myblob = new Blob( [ editor.getValue() ], {
+                type: 'text/plain' } );
+  
+   formData.append( "test", myblob, "test.prg" );
+   xhr.onreadystatechange = function() { 
+     if( this.readyState == XMLHttpRequest.DONE && this.status == 200 ) {
+        ShowResult( this.responseText );
+    } 
+   };
+   xhr.open( "POST", 'https://www.fivetechsoft.com/xcloud/run.php' );
+   xhr.send( formData );
+}
+  
+function ShowResult( cResult )
+{  
+   var div1 = document.createElement( "div" );
+   var div2 = document.createElement( "div" );
+   var div3 = document.createElement( "div" );
+   var div4 = document.createElement( "div" );
+   var div5 = document.createElement( "div" );
+   var div6 = document.createElement( "div" );
+   var cAction;
+
+   div1.className = "modal fade";
+   div1.id = "msginfo";
+
+   div2.className = "modal-dialog";
+   div1.appendChild( div2 );
+
+   div3.className = "modal-content";
+   div2.appendChild( div3 );
+   
+   div4.className = "modal-header";
+   div3.appendChild( div4 );
+   div4.innerHTML = "<button type='button' class='close' data-dismiss='modal'>&times;</button>" +
+                    "<h4 class='modal-title'>Information</h4>";
+
+   div5.className = "modal-body";
+   div3.appendChild( div5 );
+   div5.innerHTML = "<p>" + cResult + "</p>";
+
+   div6.className = "modal-footer";
+   div3.appendChild( div6 );
+   cAction = "$('#msginfo').modal('hide');";
+   div6.innerHTML = "<button type='button' class='btn btn-default' data-dismiss='modal' " + 
+                    "onclick=" + cAction + ">Close</button>";
+
+   document.body.appendChild( div1 );
+   $('#msginfo').modal('show');  
+}
