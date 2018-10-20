84019212ff8c84e1b36882eff33144c79bf63036
FiveTech Software <alinares@fivetechsoft.com>
2018-10-20 22:34:32 +0200

    Update xcloud.js

--- source/js/xcloud.js
+++ source/js/xcloud.js
@@ -43,7 +43,7 @@ function Run()
    formData.append( "test", myblob, "test.prg" );
    xhr.onreadystatechange = function() { 
      if( this.readyState == XMLHttpRequest.DONE && this.status == 200 ) {
-        ShowResult( this.responseText.substring( this.responseText.indexof( "<html>" ) ) );
+        ShowResult( this.responseText.substring( this.responseText.indexOf( "<html>" ) ) );
     } 
    };
    xhr.open( "POST", 'https://www.fivetechsoft.com/xcloud/run.php' );
