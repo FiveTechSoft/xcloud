026dec7d6def386602967484e7531d763e6aeba7
FiveTech Software <alinares@fivetechsoft.com>
2019-06-02 08:51:33 +0200

    Update xcloud.js

--- source/js/xcloud.js
+++ source/js/xcloud.js
@@ -78,7 +78,7 @@ function MsgInfo( cMsg, cTitle )
 
    div6.className = "modal-footer";
    div3.appendChild( div6 );
-   cAction = "$('#msginfo').modal('hide').data( 'bs.modal', null );";
+   cAction = "$('#msginfo').modal('hide').data('bs.modal',null);";
    div6.innerHTML = "<button type='button' class='btn btn-default' data-dismiss='modal' " + 
                     "onclick=" + cAction + ">Close</button>";
 
