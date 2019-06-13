2f276b140521fd2c471b34d8fff8d7c0bd1788c1
FiveTech Software <alinares@fivetechsoft.com>
2019-06-13 09:40:03 +0200

    Update xcloud.js

--- source/js/xcloud.js
+++ source/js/xcloud.js
@@ -84,6 +84,51 @@ function MsgInfo( cMsg, cTitle )
    $('#msginfo').modal('show');  
 }
 
+function MsgGet( cMsg, cTitle )
+{  
+   var div1 = document.createElement( "div" );
+   var div2 = document.createElement( "div" );
+   var div3 = document.createElement( "div" );
+   var div4 = document.createElement( "div" );
+   var div5 = document.createElement( "div" );
+   var div6 = document.createElement( "div" );
+   var cAction;
+
+   if( ! cTitle )
+      cTitle = "Information";
+   
+   div1.className = "modal fade";
+   div1.id = "msgget";
+
+   div2.className = "modal-dialog";
+   div1.appendChild( div2 );
+
+   div3.className = "modal-content";
+   div2.appendChild( div3 );
+   
+   div4.className = "modal-header";
+   div3.appendChild( div4 );
+   div4.innerHTML = "<button type='button' class='close' data-dismiss='modal'>&times;</button>" +
+                    "<h4 class='modal-title'>" + cTitle + "</h4>";
+
+   div5.className = "modal-body";
+   div3.appendChild( div5 );
+   div5.innerHTML = '<table class="table table-striped table-bordered"><tr style="background-color:#c0c0c0"><td>' + 
+                    '<p align="right">' + cMsg + "</p></td><td><input type='text' id='get'>" + "</td></tr></table>";
+
+   div6.className = "modal-footer";
+   div3.appendChild( div6 );
+   cAction = "document.getElementById('msgget').remove();";
+   div6.innerHTML = "<button type='button' class='btn btn-default' data-dismiss='modal' style='width:90px'" + 
+                    "onclick=" + cAction + ">Ok</button>" + 
+                    "<button type='button' class='btn btn-default' data-dismiss='modal' style='width:90px'" + 
+                    "onclick=" + cAction + ">Cancel</button>";
+
+   document.body.appendChild( div1 );
+   $('#msgget').modal('show');  
+   $('#get').focus();
+}
+
 function Result( cMsg )
 {  
    var div1 = document.createElement( "div" );
