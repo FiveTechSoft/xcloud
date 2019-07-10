ea53a04ac05337408494207cd2ebf98d21f5586a
FiveTech Software <antonio.fivetech@gmail.com>
2019-07-10 20:16:28 +0200

    Update xcloud.js

--- source/js/xcloud.js
+++ source/js/xcloud.js
@@ -130,6 +130,50 @@ function MsgGet( cMsg, cTitle )
    $('#get').focus();
 }
 
+function MsgYesNo( cMsg, cTitle, cOnYes )
+{  
+   var div1 = document.createElement( "div" );
+   var div2 = document.createElement( "div" );
+   var div3 = document.createElement( "div" );
+   var div4 = document.createElement( "div" );
+   var div5 = document.createElement( "div" );
+   var div6 = document.createElement( "div" );
+   var cAction;
+            
+   if( ! cTitle )
+      cTitle = "Please select";
+               
+   div1.className = "modal fade";
+   div1.id = "msgyesno";
+            
+   div2.className = "modal-dialog";
+   div2.style.marginTop = "300px";
+   div1.appendChild( div2 );
+            
+   div3.className = "modal-content";
+   div2.appendChild( div3 );
+               
+   div4.className = "modal-header";
+   div3.appendChild( div4 );
+   div4.innerHTML = "<button type='button' class='close' data-dismiss='modal'>&times;</button>" +
+                    "<h4 class='modal-title'>" + cTitle + "</h4>";
+            
+   div5.className = "modal-body";
+   div3.appendChild( div5 );
+   div5.innerHTML = cMsg;
+            
+   div6.className = "modal-footer";
+   div3.appendChild( div6 );
+   cAction = "document.getElementById('msgyesno').remove();";
+   div6.innerHTML = "<button type='button' class='btn btn-default' data-dismiss='modal' " + 
+                    "onclick=" + cAction + ";" + cOnYes + ' style="width:100px;">Yes</button>' + 
+                    "<button type='button' class='btn btn-default' data-dismiss='modal' " + 
+                    "onclick=" + cAction + ' style="width:100px;">No</button>';
+            
+   document.body.appendChild( div1 );
+   $('#msgyesno').modal('show');
+}
+
 function Result( cMsg )
 {  
    var div1 = document.createElement( "div" );
