bda1513445743373b1a307b4bd44de4252395055
FiveTech Software <antonio.fivetech@gmail.com>
2019-08-03 00:06:20 +0200

    Update mode-c_cpp.js

--- src-noconflict/mode-c_cpp.js
+++ src-noconflict/mode-c_cpp.js
@@ -60,7 +60,7 @@ var c_cppHighlightRules = function() {
 
     var keywordControls = (
         "break|case|continue|default|do|else|for|goto|if|_Pragma|" +
-        "CLASS|ENDCLASS|DATA|CLASSDATA|INIT|INLINE|METHOD|function|local|next|end|endif|return|switch|while|catch|operator|try|throw|using"
+        "CLASS|ENDCLASS|DATA|CLASSDATA|CLASSVAR|CONSTRUCTOR|INIT|INLINE|METHOD|function|local|next|end|endif|return|switch|while|catch|operator|try|throw|using"
     );
     
     var storageType = (
