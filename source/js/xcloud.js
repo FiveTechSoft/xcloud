249bb2c4f5bacf98c54c4c173288368069c6a1c4
FiveTech Software <antonio.fivetech@gmail.com>
2021-12-30 22:26:42 +0100

    Update xcloud.js

--- source/js/xcloud.js
+++ source/js/xcloud.js
@@ -39,7 +39,7 @@ function Run()
    o[ 'params' ] = sessionStorage.getItem( 'params' );
    console.log( 'PARAM', o );
             
-   $.post( "run.prg", o )
+   $.post( "modharbour?run.prg", o )
       .done( function( data ) { console.log( 'DONE', data ); $('#output').html( data );
                                 sessionStorage.setItem( 'code', editor.getValue() );
                                 sessionStorage.setItem( 'result', data ); } )
