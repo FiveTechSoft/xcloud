0982b93e8b9b1c0a3a38539d53bfc1317abf339e
FiveTech Software <antonio.fivetech@gmail.com>
2022-01-16 10:19:29 +0100

    Update xcloud.js

--- source/js/xcloud.js
+++ source/js/xcloud.js
@@ -60,7 +60,7 @@ function RunBH()
    console.log( 'PARAM', o );
             
    $.ajax({
-      url: "counter/run.php",
+      url: "www.fivetechsoft.com/counter/run.php",
       type: "POST",
       headers: { "Content-type": "form-data" },
       data: o,
